8e9308b6f629c90b8fec9ad9b4308e9ebbca6676
A667459 A667459 <a667459@Chaiwat-2.local>
2024-01-06 13:22:21 +0700

    collect data as slice and write to json

--- go/main.go
+++ go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -11,34 +12,61 @@ import (
 )
 
 type student struct {
-	Lastname  string
-	Firstname string
-	SSN       string
-	Test1     float64
-	Test2     float64
-	Test3     float64
-	Test4     float64
-	Final     float64
-	Grade     string
+	Lastname  string  `json:"lastname"`
+	Firstname string  `json:"firstname"`
+	SSN       string  `json:"ssn"`
+	Test1     float64 `json:"test1"`
+	Test2     float64 `json:"test2"`
+	Test3     float64 `json:"test3"`
+	Test4     float64 `json:"test4"`
+	Final     float64 `json:"final"`
+	Grade     string  `json:"grade"`
 }
 
 func main() {
-	//Open file
 	csvfile, err := os.Open("../homework/api/rawdata/grades.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer csvfile.Close()
 
 	r := csv.NewReader(csvfile)
 	r.LazyQuotes = true
+	students := readCsvFile(r)
+
+	for _, student := range students {
+		fmt.Println(student)
+	}
+
+	bytes, _ := json.MarshalIndent(students, "", " ")
+	fmt.Println(string(bytes))
+
+	writeJsonFile(students)
+
+}
+
+func writeJsonFile(students []student) {
+	file, err := os.Create("../homework/json/students.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(students)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readCsvFile(r *csv.Reader) []student {
+	students := []student{}
 	const expectedRecord int = 9
 	lineCount := 0
-	students := []student{}
-	// skip first line
 
 	for {
 		record, err := r.Read()
-		if lineCount == 0 {
+		if lineCount == 0 { // skip the first line
 			lineCount++
 			continue
 		}
@@ -74,20 +102,28 @@ func main() {
 			fmt.Println(err)
 		}
 		students = append(students, student{
-			Lastname:  record[0],
-			Firstname: record[1],
-			SSN:       record[2],
+			Lastname:  deleteQuotes(strings.TrimSpace(record[0])),
+			Firstname: deleteQuotes(strings.TrimSpace(record[1])),
+			SSN:       deleteQuotes(strings.TrimSpace(record[2])),
 			Test1:     test1,
 			Test2:     test2,
 			Test3:     test3,
 			Test4:     test4,
 			Final:     final,
-			Grade:     record[8],
+			Grade:     deleteQuotes(strings.TrimSpace(record[8])),
 		})
 		lineCount++
 	}
 
-	for _, student := range students {
-		fmt.Println(student)
+	return students
+}
+
+func deleteQuotes(s string) string {
+	if len(s) > 0 && s[0] == '"' {
+		s = s[1:]
 	}
+	if len(s) > 0 && s[len(s)-1] == '"' {
+		s = s[:len(s)-1]
+	}
+	return s
 }
